4b111f9626a9f2e1a9551ff48d9dd3325fc1e7a8
DangDuong1998 <mvstarkfinal.ultra@gmail.com>
2025-05-26 20:51:23 +0900

    [Bài tập] Xây dựng lớp Rectangle

---
+++ bt_01/src/bt_01/Main.java
@@ -0,0 +1,16 @@
+package bt_01;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    Rectangle rect1 = new Rectangle();
+    System.out.println("Hình chữ nhật 1: ");
+    rect1.display();
+
+    Rectangle rect2 = new Rectangle(5,3);
+    System.out.println("Hình chữ nhật 2: ");
+    rect2.display();
+    }
+
+}
